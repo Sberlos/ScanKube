0f6d543043a6b8ea849a2a5b3aa2f1856e1ccd8e
Francesco Berla <francesco.berla@usi.ch>
2020-04-29 18:07:50 +0200

    Add prototype of cli interface using kube-hunter
    Add the skeleton of the cli that will execute all the tools.
    Add the implementation for kube-hunter: it's a simple implementation
    that invokes kubectl directly and extract it's output in json format in
    order to manipulate it and show it at later stage.
    Json has been chosen because it's a standard format that can be easily
    parsed and it's the same of the output of mkit.

---
+++ Francesco/Code/prototype.py
@@ -0,0 +1,96 @@
+import argparse
+import subprocess
+import time
+
+def runTool(tool):
+    if tool == "cis":
+        runCis()
+    elif tool == "kube-hunter":
+        runHunter()
+    elif tool == "kubesec":
+        runKubesec()
+    elif tool == "mkit":
+        runMkit()
+    elif tool == "custom":
+        runMkit()
+    else:
+        print("Unexpected tool") #Shouldn't happen as there is the check before
+
+def runCis():
+    pass
+
+"""
+We have two options: run the job and then delete it or check for its existence
+and rerun it when needed
+"""
+def runHunter():
+    # Basic invokation of kubectl, we could find better ways using the API
+    trash = subprocess.run(["kubectl", "create", "-f", "kube-hunter-job-json.yaml"])
+    time.sleep(5) #Seems to be needed, I have to check
+
+    """
+    describeOutP = subprocess.run(["kubectl", "describe", "job",
+        "kube-hunter-json-test"], capture_output=True, text=True)
+    """
+    describeOutP = subprocess.run(["kubectl", "describe", "pods",
+        "kube-hunter-json-test"], capture_output=True, text=True)
+    describeOut = describeOutP.stdout #not handling errors for now
+    podName = extractJobNamePods(describeOut)
+
+    time.sleep(25) #hardcoded, ideally we should keep describing/checking until completed
+    podLogP = subprocess.run(["kubectl", "logs", podName], capture_output=True,
+            text=True)
+    podLog = podLogP.stdout #not handling errors for now
+
+    jsonReport = extractJson(podLog)
+    return jsonReport
+
+def extractJobNameJob(output):
+    lines = output.splitlines()
+    podName = lines[-1].split()[-1] #check if right
+    print(podName)
+    return podName
+
+def extractJobNamePods(output):
+    lines = output.splitlines()
+    podName = lines[0].split()[1] #check if right
+    print(podName)
+    return podName
+
+def extractJson(log):
+    start = log.find('{')
+    if start == -1:
+        return "" # add proper error mechanism
+    json = log[start:]
+    print(json)
+    return json
+
+def runKubesec():
+    pass
+
+def runMkit():
+    pass
+
+def runCustom():
+    pass
+
+# Command line parser logic
+def parsing():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tool", choices=["cis", "kube-hunter", "kubesec", "mkit",
+        "custom"], help="select the tool to run")
+    parser.add_argument("-v", "--verbosity", action="count", default=0,
+        help="increase output verbosity")
+    args = parser.parse_args()
+    if args.verbosity >= 2:
+        print("performing a check using {}".format(args.tool))
+        print("More descriptive stuff to be determined")
+        runTool(args.tool)
+    elif args.verbosity >= 1:
+        print("performing a check using {}".format(args.tool))
+        runTool(args.tool)
+    else:
+        runTool(args.tool)
+
+if __name__ == '__main__':
+    parsing()
