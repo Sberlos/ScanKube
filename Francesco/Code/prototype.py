bd722850a1c2dc3190f574e4bdce2de2b9bbecb4
Francesco Berla <francesco.berla@usi.ch>
2020-05-03 22:36:06 +0200

    Add kube-bench for cisbenchmark
    Add kube-bench as implementation of cis benchmark.
    Add first stub of interaction with Kubernetes API directly using the
    official library. I will probably transform every invokation of kubectl
    in calls to the API.
    The integration of kube-bench is only partial: we launch the Pod and we
    execute the job but we are unable to extract the Pod name in a nice way
    for retrieving the logs.

--- Francesco/Code/prototype.py
+++ Francesco/Code/prototype.py
@@ -3,11 +3,17 @@ import subprocess
 import time
 import requests
 import threading
+from os import path
 #import psutil
+#import docker
+from kubernetes import client, config
+import yaml
+import json
 
 def runTool(tool):
+    """Decide which tool to run based on the argument"""
     if tool == "cis":
-        runCis()
+        runCisBench()
     elif tool == "kube-hunter":
         runHunter()
     elif tool == "kubesec":
@@ -19,14 +25,76 @@ def runTool(tool):
     else:
         print("Unexpected tool") #Shouldn't happen as there is the check before
 
-def runCis():
-    pass
+def runCisBench():
+    ns = "default"
+    config.load_kube_config()
+    batch_v1 = client.BatchV1Api()
+    core_v1 = client.CoreV1Api()
+    with open(path.join(path.dirname(__file__), "kubebatch-job.yaml")) as f:
+        job = yaml.safe_load(f)
+        resJob = batch_v1.create_namespaced_job(namespace=ns, body=job)
+
+    time.sleep(10) #In the future I will check the status
+    # The above could be replaced by a loop on resJob.status
+
+    #### Cannot parse a json string to a dict... ####
+    # The idea is to parse the json to extract the UID to pass to list
+    # namespace event in order to find the name of the pod that ran the job in
+    # order to retrive its logs
+    #
+    # If the json way doens't work I can do using string manipulation but it's
+    # terrible
+
+    #print(str(resJob))
+    #jobDict = json.loads(json.dumps(str(resJob).replace("\'", "\"")))
+    jobDict = eval(json.dumps(str(resJob).replace("\'", "\"")))
+    print(jobDict)
+    print(type(jobDict))
+
+    """
+    listEvents = core_v1.list_namespaced_event(ns,
+    field_selector="involvedObject.name=kube-bench")
+    print(listEvents)
+    """
+    """
+    descriptionJob = core_v1.read_namespaced_event("kube-bench", ns)
+    print(descriptionJob)
+    """
+    """
+    listJob = batch_v1.list_namespaced_job(namespace=ns)
+    print(listJob)
+    podName = extractName(resJob.metadata)
+
+    log = core_v1.read_namespaced_pod_log(name=podName, namespace=ns)
+    print(log)
+    return log
+    """
+
+def runCisBenchDocker():
+    """Run aqua-bench (an implementation of the cis benchmark) as Docker
+    container
+    """
+    client = docker.from_env()
+    kubectlPath = "/usr/bin/kubectl" #hardcoded, can be found with which
+    volumesDict = {"/etc": {"bind": "/etc", "mode": "ro"},
+            "/var": {"bind": "/var", "mode": "ro"},
+            kubectlPath: {"bind": "/usr/local/mount-from-host/bin/kubectl",
+                "mode": "ro"},
+            "~/.kube": {"bind": "/.kube", "mode": "ro"}}
+    log = client.containers.run("aquasec/kube-bench:latest", command="node",
+            environment=["KUBECONFIG=/.kube/config"], pid_mode="host",
+            tty=False, volumes=volumesDict)
+    print(log)
 
 """
 We have two options: run the job and then delete it or check for its existence
 and rerun it when needed
 """
 def runHunter():
+    """Run kube-hunter
+    Creates a Pod in the cluster used to run a Job that will scan the
+    environment
+    """
     # Basic invokation of kubectl, we could find better ways using the API
     subprocess.run(["kubectl", "create", "-f", "kube-hunter-job-json.yaml"])
     time.sleep(5) #Seems to be needed, I have to check
@@ -49,18 +117,26 @@ def runHunter():
     return jsonReport
 
 def extractJobNameJob(output):
+    """Extract and return the Job name tied to kube-hunter from a kubectl
+    describe jobs command output
+    """
     lines = output.splitlines()
     podName = lines[-1].split()[-1] #check if right
     print(podName)
     return podName
 
 def extractJobNamePods(output):
+    """Extract and return the Job name tied to kube-hunter from a kubectl
+    describe pods command output
+    """
     lines = output.splitlines()
     podName = lines[0].split()[1] #check if right
     print(podName)
     return podName
 
 def extractJson(log):
+    """Extract and return the json output from the kube-hunter scan from the
+    Pod logs"""
     start = log.find('{')
     if start == -1:
         return "" # add proper error mechanism
@@ -72,6 +148,9 @@ def runKubesec():
     pass
 
 def runMkit():
+    """Run the mkit tool
+    Returns the output in json format
+    """
     # For the moment we assume that the tool has already been downloaded
     server = threading.Thread(target=serverRun)
     server.start()
@@ -82,12 +161,17 @@ def runMkit():
     return json
 
 def serverRun():
+    """Run the mkit server
+    At the moment is just a simple call to make
+    """
     subprocess.run(["make", "run-k8s"])
+
 def runCustom():
     pass
 
 # Command line parser logic
 def parsing():
+    """Parse the command line arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument("tool", choices=["cis", "kube-hunter", "kubesec", "mkit",
         "custom"], help="select the tool to run")
